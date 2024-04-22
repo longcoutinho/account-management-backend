93c003c80b7a49b823f36254899eba004868922d
longcoutinho@gmail.com <longdeptraiii2k>
2024-04-22 17:21:36 +0700

    jar file

--- src/main/java/com/example/demo/services/topupgame/GamotaService.java
+++ src/main/java/com/example/demo/services/topupgame/GamotaService.java
@@ -16,10 +16,10 @@ public class GamotaService {
 
     public String getUsernameById(String id) {
         Map<String, String> params = new HashMap<>();
-        params.put("paygate_id", "66215974d01c4a2c4e42c654");
+        params.put("paygate_id", "6621acd940d7aa5bfc7ba2c0");
         params.put("serverID", "1051169902");
         params.put("role", id);
-        String res = FnCommon.doPostRequestWithCookie(searchUrl, params, null, "_ga=GA1.1.2031674636.1712518082; PHPSESSID=1211a17d61c69e21a3218cbed4d1d310; _ga_S01HFLJJM6=GS1.1.1713461269.3.1.1713461623.0.0.0");
+        String res = FnCommon.doPostRequestWithCookie(searchUrl, params, null, "_gid=GA1.2.1123368069.1713482968; _gat=1; _fbp=fb.1.1713482967877.2078034002; _ga=GA1.1.2031674636.1712518082; PHPSESSID=84a03136a239304c54394176c32c5bce; _ga_VE1NT14GF5=GS1.1.1713482967.1.0.1713482969.58.0.0; _ga_S01HFLJJM6=GS1.1.1713482969.4.0.1713482974.0.0.0");
         if (res == null) throw new CustomException(ErrorApp.IGG_INVALID_ID);
         int index = res.indexOf("<option");
         if (index == -1) {
